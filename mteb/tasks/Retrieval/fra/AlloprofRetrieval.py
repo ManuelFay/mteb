17f66c75fb9d917b5bc19755c8617d14e241abc9
ManuelFay <manuel.faysse@illuin.tech>
2024-09-20 00:57:40 +0200

    hackk

--- mteb/tasks/Retrieval/fra/AlloprofRetrieval.py
+++ mteb/tasks/Retrieval/fra/AlloprofRetrieval.py
@@ -1,10 +1,29 @@
 from __future__ import annotations
-
+import os
 import datasets
 
 from mteb.abstasks.TaskMetadata import TaskMetadata
 
 from ....abstasks.AbsTaskRetrieval import AbsTaskRetrieval
+
+
+def load_dataset(**kwargs):
+    print(f"Loading dataset with kwargs: {kwargs}")
+    dataset_name = kwargs.pop("path").split("/")[-1]
+    print("Loading dataset", dataset_name)
+    if os.path.exists(
+            "/lus/scratch/CT6/c1615122/SHARED/data/eval_datasets_hf/" + dataset_name
+    ):
+        print(
+            "Loading dataset from local storage at /lus/scratch/CT6/c1615122/SHARED/data/eval_datasets_hf/"
+        )
+        return datasets.load_dataset(
+            "/lus/scratch/CT6/c1615122/SHARED/data/eval_datasets_hf/" + dataset_name,
+            **kwargs,
+        )
+    else:
+        print("Loading dataset from Hugging Face")
+        return datasets.load_dataset(**kwargs)
 
 
 class AlloprofRetrieval(AbsTaskRetrieval):
@@ -58,11 +77,11 @@ class AlloprofRetrieval(AbsTaskRetrieval):
         if self.data_loaded:
             return
         # fetch both subsets of the dataset
-        corpus_raw = datasets.load_dataset(
+        corpus_raw = load_dataset(
             name="documents",
             **self.metadata_dict["dataset"],
         )
-        queries_raw = datasets.load_dataset(
+        queries_raw = load_dataset(
             name="queries",
             **self.metadata_dict["dataset"],
         )
