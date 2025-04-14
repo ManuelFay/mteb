48da40df340ec314a2caac03da7ba7be8c7d2d19
manuel <manuel.faysse@illuin.tech>
2025-04-14 13:47:56 +0200

    remove

--- mteb/models/colpali_engine_models.py
+++ mteb/models/colpali_engine_models.py
@@ -89,7 +89,7 @@ class ColPaliEngineWrapper:
             return torch.cat(all_image_embeddings, dim=0)
 
     def calculate_probs(self, text_embeddings, image_embeddings):
-        logits = self.processor.score_single_vector(text_embeddings, image_embeddings)
+        logits = torch.einsum("bd,cd->bc", text_embeddings, image_embeddings).to(torch.float32)
         probs = (logits * 100).softmax(dim=-1)
         return probs
 
